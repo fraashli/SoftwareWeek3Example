4d647e86e043e3c10fd7b8aab7069e7d918b76ea
fraashli <65675045+fraashli@users.noreply.github.com>
2020-06-03 11:54:54 -0400

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -16,7 +16,8 @@ public class Week3 {
      */
     public static void main(String[] args) {
         System.out.println("learning github");
-        System.out.println("learning github1");
+        System.out.println("learning github push");
+        System.out.println("learning github pull");   
         
     }
     
